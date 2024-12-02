018e2811d0edfb45d9f716b8c24fedb7d6bac3cd
notKamui <teillard.jimmy@outlook.fr>
2024-12-02 21:34:17 +0100

    feat: enhance AppSidebar with navigation items and improved layout

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,18 +1,56 @@
+import { title } from '@app/components/ui/primitives/typography'
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from '@app/components/ui/sidebar'
+import { Link } from '@tanstack/react-router'
+import { HomeIcon, LogInIcon } from 'lucide-react'
+
+const items = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: HomeIcon,
+  },
+  {
+    title: 'Login',
+    url: '/login',
+    icon: LogInIcon,
+  },
+] as const
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <h1 className={title({ h: 2 })}>Time Recorder</h1>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
