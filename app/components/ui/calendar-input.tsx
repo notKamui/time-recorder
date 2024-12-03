f26d9c7007275321dba4a7c41dd256b722dffb49
notKamui <teillard.jimmy@outlook.fr>
2024-12-03 18:32:54 +0100

    feat: add CalendarForm component with popover functionality and date selection

---
+++ app/components/ui/calendar-input.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Button } from '@app/components/ui/button'
+import { Calendar } from '@app/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@app/components/ui/popover'
+import { cn } from '@app/utils/cn'
+import { Time } from '@common/utils/time'
+import { CalendarIcon } from 'lucide-react'
+
+export type CalendarFormProps = {
+  value?: Date
+  onChange: (date?: Date) => void
+}
+
+export function CalendarForm({ value, onChange }: CalendarFormProps) {
+  return (
+    <>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={'outline'}
+            className={cn(
+              'w-[240px] pl-3 text-left font-normal',
+              !value && 'text-muted-foreground',
+            )}
+          >
+            {value ? Time.from(value).formatDay() : <span>Pick a date</span>}
+            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={value}
+            onSelect={onChange}
+            disabled={(date) =>
+              date > new Date() || date < new Date('1900-01-01')
+            }
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    </>
+  )
+}
