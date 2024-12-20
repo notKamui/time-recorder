42d228d76854c4728fb4e586bc4131d5d9cda64c
notKamui <teillard.jimmy@outlook.fr>
2024-12-20 16:08:13 +0100

    feat(cursor): enhance cursor state management with dynamic variants and caret height adjustment

--- app/components/cursor.tsx
+++ app/components/cursor.tsx
@@ -1,11 +1,14 @@
 import { useTheme } from '@app/hooks/use-theme'
-import { type SpringOptions, motion, useSpring } from 'motion/react'
+import { type SpringOptions, type Variants, motion, useMotionValue, } from 'motion/react'
 import { useEffect, useRef, useState } from 'react'
 
 const springConfig = {
   stiffness: 1000,
   damping: 70,
+  velocity: 1000
 } satisfies SpringOptions
+
+
 
 function hasButtonOrAnchorAncestor(element: HTMLElement | null): boolean {
   if (!element) return false
@@ -26,8 +29,23 @@ export function Cursor() {
     }
   }, [])
 
+
+  const [caretHeight, setCaretHeight] = useState(0)
+  const variants = {
+    default: {
+    },
+    pointer: {
+      scale: 2.5,
+    },
+    text: {
+      height: caretHeight,
+      width: 2,
+      borderRadius: 0,
+    }
+  } satisfies Variants
+
   const cursor = useRef<HTMLDivElement>(null)
-  const [isHovering, setIsHovering] = useState(false)
+  const [state, setState] = useState<keyof typeof variants>('default')
 
   const storedPosition =
     typeof localStorage !== 'undefined' &&
@@ -36,20 +54,25 @@ export function Cursor() {
     storedPosition ? JSON.parse(storedPosition) : { x: 0, y: 0 },
   )
 
-  const positionX = useSpring(initialPosition.current.x, springConfig)
-  const positionY = useSpring(initialPosition.current.y, springConfig)
+  const positionX = useMotionValue(initialPosition.current.x)
+  const positionY = useMotionValue(initialPosition.current.y)
 
   useEffect(() => {
     function handleMouseMove(e: MouseEvent) {
       if (!cursor.current) return
 
-      const isHovered =
-        e.target instanceof HTMLElement && hasButtonOrAnchorAncestor(e.target)
-      setIsHovering(isHovered)
+      const isHovered = e.target instanceof HTMLElement && hasButtonOrAnchorAncestor(e.target)
+      const postion = document.caretPositionFromPoint(e.clientX, e.clientY)
+      const isOverText = !!postion && postion.offsetNode.nodeType === Node.TEXT_NODE && postion.offsetNode.parentElement === e.target
+
+      const state = isHovered ? 'pointer' : isOverText ? 'text' : 'default'
+      setState(state)
+      if (state === 'text') {
+        setCaretHeight(postion?.getClientRect()?.height || 0)
+      }
 
       positionX.set(e.clientX)
       positionY.set(e.clientY)
-
       localStorage.setItem(
         'cursor-position',
         JSON.stringify({ x: e.clientX, y: e.clientY }),
@@ -65,23 +88,26 @@ export function Cursor() {
     }
   }, [positionX.set, positionY.set])
 
-  return (<motion.div
-    ref={cursor}
-    style={{
-      position: 'fixed',
-      left: positionX,
-      top: positionY,
-      pointerEvents: 'none',
-      zIndex: 9999,
-      translateX: '-50%',
-      translateY: '-50%',
-      transition: "transform 0.2s ease, opacity 0.2s ease",
-      scale: isHovering ? 2.5 : 1,
-      width: 20,
-      height: 20,
-      borderRadius: '50%',
-      backgroundColor: theme === 'dark' ? 'hsl(var(--primary))' : 'hsl(var(--secondary))',
-      mixBlendMode: 'difference',
-    }}
-  />)
+  return (
+    <motion.div
+      ref={cursor}
+      variants={variants}
+      animate={state}
+      style={{
+        position: 'fixed',
+        left: positionX,
+        top: positionY,
+        pointerEvents: 'none',
+        zIndex: 9999,
+        translateX: '-50%',
+        translateY: '-50%',
+        width: 20,
+        height: 20,
+        borderRadius: '50%',
+        backgroundColor:
+          theme === 'dark' ? 'hsl(var(--primary))' : 'hsl(var(--secondary))',
+        mixBlendMode: 'difference',
+      }}
+    />
+  )
 }
